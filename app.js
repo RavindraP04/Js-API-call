420d0412d1c17dbbd771efde696f7b5568e829d0
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-01-06 22:42:16 +0530

    Improved:text copy/paste

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var heading = document.querySelector(".head");
-console.log(heading);
+var btnTranslate = document.querySelector("#btn-translate");
+var inputTxt = document.querySelector("#txtInput")
+var outputTxt = document.querySelector("#text-output");
+// console.log(heading);
 
-var message = prompt("What is your name? ");
-alert("Welcome "+ message);
+// var message = prompt("What is your name? ");
+// alert("Welcome "+ message);
+
+btnTranslate.addEventListener('click', clickHandler)
+
+function clickHandler(){
+    console.log("clicked")
+    console.log("Input:- " + inputTxt.value)
+    outputTxt.innerText = inputTxt.value
+}
